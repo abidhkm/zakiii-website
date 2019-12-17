34afbac08b11e896dca692e0dd0cf4f0ea6966b7
Zakaria Mansouri <zaqo.cs@outlook.com>
2019-12-17 15:32:16 +0100

    config done right

---
+++ frontend/src/apps/main/config/app/index.ts
@@ -0,0 +1,20 @@
+import { fullstackConfig } from "config";
+import { frontendConfig } from "src/config";
+import { language } from "../language";
+
+const isDev = frontendConfig.environment.isDev;
+const host = (typeof window !== "undefined") ? window.location.host : (isDev ? "127.0.0.1" : "www.zakiii.com");
+const protocol = (typeof window !== "undefined") ? window.location.protocol : (isDev ? "http" : "https");
+const origin = protocol + "//" + host;
+
+const frontendBaseURL = language.isDefaultLanguage ? "" : "/" + language.languageCode;
+
+export const app = {
+  host,
+  origin,
+  protocol,
+
+  backendURL: fullstackConfig.app(isDev),
+  frontendBaseURL,
+  frontendURL: origin + frontendBaseURL,
+};
--- fullstack/src/config/app/index.js
+++ fullstack/src/config/app/index.js
@@ -1,5 +1,5 @@
 // FIXME: ts doesn't load code outside of baseUrl!
 export const app = (isDev) => ({
-  backendURL: isDev ? "http://127.0.0.1:9090" : "https://api.dts.zakiii.com",
-  frontendURL: isDev ? "http://127.0.0.1:8080" : "https://dts.zakiii.com",
+  backendURL: isDev ? "http://127.0.0.1:9090" : "https://api.zakiii.com",
+  frontendURL: isDev ? "http://127.0.0.1:8080" : "https://zakiii.com",
 });
