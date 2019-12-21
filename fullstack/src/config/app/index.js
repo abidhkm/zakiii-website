d8a7142638dc8effc522312fdb8011cba5b6284a
Zakaria Mansouri <zaqo.cs@outlook.com>
2019-12-21 14:43:26 +0100

    updated backend config

--- fullstack/src/config/app/index.js
+++ fullstack/src/config/app/index.js
@@ -1,5 +1,5 @@
 // FIXME: ts doesn't load code outside of baseUrl!
 export const app = (isDev) => ({
-  backendURL: isDev ? "http://127.0.0.1:9090" : "https://api.zakiii.com",
-  frontendURL: isDev ? "http://127.0.0.1:8080" : "https://zakiii.com",
+  backendURL: isDev ? "http://127.0.0.1:9090" : "https://data.zakiii.com",
+  frontendURL: isDev ? "http://127.0.0.1:8080" : "https://www.zakiii.com",
 });
