9327d0d095094091d545a9ca065c316722882ec1
Zakaria Mansouri <zaqo.cs@outlook.com>
2019-12-21 14:33:05 +0100

    added data backend

---
+++ backend/build.js
@@ -0,0 +1,15 @@
+// clean out folder
+const fs = require('fs-extra');
+fs.removeSync("./out");
+
+// run all builders
+require("./builders");
+
+// if in dev environment, run express
+if (process.env.NODE_ENV === "development") {
+  const express = require('express');
+  const app = express();
+  const port = 9090;
+  app.use(express.static('out'));
+  app.listen(port, () => console.log(`Backend listening on port ${port}!`));
+}
