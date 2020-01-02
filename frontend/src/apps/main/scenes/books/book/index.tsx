fefad6cefaab338e44376ef2f4ae2b25a3019b56
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-01-02 21:43:12 +0100

    added comment section to books

--- frontend/src/apps/main/scenes/books/book/index.tsx
+++ frontend/src/apps/main/scenes/books/book/index.tsx
@@ -4,33 +4,48 @@ import Markdown from "react-markdown";
 import { Book } from "t9/apps/main/types";
 import "./style";
 
-export const BookSene: React.FC<{ book?: Book | null }> = ({ book }) => {
-
-  if (book) {
-    document.title = book.title + " | zakiii";
+export class BookSene extends React.Component<{ book?: Book | null }, {}> {
+  public componentDidMount() {
+    setTimeout(() => {
+      window.FB.XFBML.parse();
+    }, 3000);
   }
 
-  return (
-    <Layout className="book">
-      {book
-        ? (
-          <React.Fragment key={book.slug}>
-            <img className="image" src={book.image} alt={book.title} />
-            <Layout.Content
-              style={{
-                background: "#fff",
-                padding: "1rem",
-              }}
-            >
-              <h1 className="title">{book.title}</h1>
-              <p className="description">{book.description}</p>
-              <Divider />
-              <Markdown source={book.content} />
-            </Layout.Content>
-          </React.Fragment>
-        )
-        : <Skeleton className="sidebar-skeleton" active={true} paragraph={{ rows: 6, width: ["80%", "70%", "60%", "50%", "90%"] }} />
-      }
-    </Layout>
-  );
-};
+  public render() {
+    const book = this.props.book;
+
+    if (book) {
+      document.title = book.title + " | zakiii";
+    }
+
+    return (
+      <Layout className="book">
+        {book
+          ? (
+            <React.Fragment key={book.slug}>
+              <img className="image" src={book.image} alt={book.title} />
+              <Layout.Content
+                style={{
+                  background: "#fff",
+                  padding: "1rem",
+                }}
+              >
+                <h1 className="title">{book.title}</h1>
+                <p className="description">{book.description}</p>
+                <Divider />
+                <Markdown source={book.content} />
+              </Layout.Content>
+              <div
+                className="fb-comments"
+                data-href={location.origin + location.pathname}
+                data-width="100%"
+                data-numposts="5"
+              />
+            </React.Fragment>
+          )
+          : <Skeleton className="sidebar-skeleton" active={true} paragraph={{ rows: 6, width: ["80%", "70%", "60%", "50%", "90%"] }} />
+        }
+      </Layout>
+    );
+  }
+}
