73dfeccb76f71bdc93672965b12584acbb96d7c1
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-03-19 16:35:32 +0400

    added COVID-19 endpoint

---
+++ frontend/functions/src/covid-19.ts
@@ -0,0 +1,17 @@
+import * as functions from 'firebase-functions';
+import axios from "axios";
+
+export const covid19 = functions.https.onRequest(async (request, response) => {
+  const gStatistics = JSON.parse((await (await axios.get("https://view.inews.qq.com/g2/getOnsInfo?name=disease_other")).data).data).globalStatis;
+  const data = JSON.parse((await (await axios.get("https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5")).data).data);
+  const cStatistics = data.chinaTotal;
+  const lastUpdateTime = data.lastUpdateTime;
+
+  response.json({
+    confirm: gStatistics.confirm + cStatistics.confirm,
+    heal: gStatistics.heal + cStatistics.heal,
+    dead: gStatistics.dead + cStatistics.dead,
+    lastUpdateTime,
+  }).end();
+
+});
