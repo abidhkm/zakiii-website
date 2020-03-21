0869d1a3c05bce0143a39be3f795f61d0d7d31e4
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-03-21 23:15:44 +0400

    added dz and usa stats

--- frontend/functions/src/covid-19.ts
+++ frontend/functions/src/covid-19.ts
@@ -7,16 +7,37 @@ export const covid19 = functions.https.onRequest(async (request, response) => {
     rp({ uri: "https://view.inews.qq.com/g2/getOnsInfo?name=disease_other", json: true }),
     rp({ uri: "https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5", json: true }),
   ]);
-  const gStatistics = JSON.parse(gRes.data).globalStatis;
+  const gData = JSON.parse(gRes.data);
   const data = JSON.parse(cRes.data);
   const cStatistics = data.chinaTotal;
   const lastUpdateTime = data.lastUpdateTime;
 
+  const dz = (gData.foreignList as Array<any>).find((country) => country.name === "阿尔及利亚");
+  const uae = (gData.foreignList as Array<any>).find((country) => country.name === "阿联酋");
+
   response.json({
-    confirm: gStatistics.confirm + cStatistics.confirm,
-    heal: gStatistics.heal + cStatistics.heal,
-    dead: gStatistics.dead + cStatistics.dead,
+    confirm: gData.globalStatis.confirm + cStatistics.confirm,
+    heal: gData.globalStatis.heal + cStatistics.heal,
+    dead: gData.globalStatis.dead + cStatistics.dead,
     lastUpdateTime: lastUpdateTime + " GMT+8",
+    countries: [
+      {
+        code: "dz",
+        name_en: "Algeria",
+        name_ar: "الجزائر",
+        confirm: dz.confirm,
+        heal: dz.heal,
+        dead: dz.dead,
+      },
+      {
+        code: "uae",
+        name_en: "Algeria",
+        name_ar: "الإمارات العربية المتحدة",
+        confirm: uae.confirm,
+        heal: uae.heal,
+        dead: uae.dead,
+      }
+    ],
   }).end();
 
 });
