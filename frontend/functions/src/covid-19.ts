f013790fe6b2953cbe0590f115ab7935520a37c0
Zakaria Mansouri <zaqo.cs@outlook.com>
2020-03-26 15:17:44 +0400

    updated datasource to totok endpoint

--- frontend/functions/src/covid-19.ts
+++ frontend/functions/src/covid-19.ts
@@ -3,45 +3,41 @@ import axios from "axios";
 
 export const covid19 = functions.https.onRequest(async (request, response) => {
 
-  const [{ data: gRes }, { data: cRes }] = await Promise.all([
-    (await axios.get("https://view.inews.qq.com/g2/getOnsInfo?name=disease_other")),
-    (await axios.get("https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5")),
-  ]);
+  const res = await axios.get("https://news.totok.ai/corov/dayinfo/rank");
+  const data = res.data.response;
 
-  const gData = JSON.parse(gRes.data);
-  const data = JSON.parse(cRes.data);
-  const cStatistics = data.chinaTotal;
-  const lastUpdateTime = data.lastUpdateTime;
+  const lastUpdate = (data.lastUpdate as string).substring((data.lastUpdate as string).indexOf(":") + 2);
 
-  const dz = (gData.foreignList as Array<any>).find((country) => country.name === "阿尔及利亚");
-  const uae = (gData.foreignList as Array<any>).find((country) => country.name === "阿联酋");
+  const all = (data.perDayInfo as Array<any>).pop();
+  const dz = (data.perDayInfo as Array<any>).find((country) => country.country === "Algeria");
+  const uae = (data.perDayInfo as Array<any>).find((country) => country.country === "UAE");
 
   response.json({
-    lastUpdateTime: lastUpdateTime + " GMT+8",
+    lastUpdateTime: lastUpdate,
     stats: [
       {
         code: "all",
         name_en: "World Wide",
         name_ar: "جميع أنحاء العالم",
-        confirm: gData.globalStatis.confirm + cStatistics.confirm,
-        heal: gData.globalStatis.heal + cStatistics.heal,
-        dead: gData.globalStatis.dead + cStatistics.dead,
+        confirm: all.totalCases.replace(/\D/g, ""),
+        heal: all.totalRecovered.replace(/\D/g, ""),
+        dead: all.totalDeath.replace(/\D/g, ""),
       },
       {
         code: "dz",
         name_en: "Algeria",
         name_ar: "الجزائر",
-        confirm: dz.confirm,
-        heal: dz.heal,
-        dead: dz.dead,
+        confirm: dz.totalCases,
+        heal: dz.totalRecovered.replace(/\D/g, ""),
+        dead: dz.totalDeath.replace(/\D/g, ""),
       },
       {
         code: "uae",
         name_en: "UAE",
         name_ar: "الإمارات العربية المتحدة",
-        confirm: uae.confirm,
-        heal: uae.heal,
-        dead: uae.dead,
+        confirm: uae.totalCases.replace(/\D/g, ""),
+        heal: uae.totalRecovered.replace(/\D/g, ""),
+        dead: uae.totalDeath.replace(/\D/g, ""),
       }
     ],
   }).end();
